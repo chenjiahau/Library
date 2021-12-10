dd2f150dadeea87a6a85b34ed1ece96bd2bcb26b
chenjiahau <ivan.chen@dlinkcorp.com>
2021-12-10 10:47:41 +0800

    Make from and to property

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-
+const calendar = Calendar(2021, 12, 8, 14, 30);
--- calendar.js
+++ calendar.js
@@ -1,25 +1,74 @@
 (function (global, $) {
-  var Calendar = function (year, month, day, hour, minute) {
-    return new Calendar.init(year, month, day, hour, minute);
-  };
-
-  Calendar.prototype = {};
-
-  Calendar.init = function (year, month, day, hour, minute) {
-    var self = this;
-
+  function transformParameter(year, month, day, hour, minute) {
     year = parseInt(year);
     month = parseInt(month);
     day = parseInt(day);
     hour = parseInt(hour) || 0;
     minute = parseInt(minute) || 0;
-    self.target = null;
 
-    if (year && month && day) {
-      self.target = new Date(year, month - 1, day, hour, minute);
+    return {
+      year,
+      month,
+      day,
+      hour,
+      minute,
+    };
+  }
+
+  var Calendar = function (year, month, day, hour, minute) {
+    return new Calendar.init(year, month, day, hour, minute);
+  };
+
+  Calendar.prototype = {
+    setTo: function (year, month, day, hour, minute) {
+      var parameter = transformParameter(year, month, day, hour, minute);
+
+      if (parameter.year && parameter.month && parameter.day) {
+        this.toDate = new Date(
+          parameter.year,
+          parameter.month,
+          parameter.day,
+          parameter.hour,
+          parameter.minute
+        );
+      }
+
+      return this;
+    },
+  };
+
+  Calendar.init = function (year, month, day, hour, minute) {
+    var self = this;
+    var parameter = transformParameter(year, month, day, hour, minute);
+
+    self.from = null;
+    self.to = null;
+    self.year = null;
+    self.month = null;
+    self.day = null;
+    self.timestampe = null;
+    self.unixTimestampe = null;
+
+    if (parameter.year && parameter.month && parameter.day) {
+      self.from = new Date(
+        parameter.year,
+        parameter.month - 1,
+        parameter.day,
+        parameter.hour,
+        parameter.minute
+      );
+      self.year = parameter.year;
+      self.month = parameter.month;
+      self.day = parameter.day;
     } else {
-      self.target = new Date();
+      self.from = new Date();
+      self.year = self.from.getFullYear();
+      self.month = self.from.getMonth() + 1;
+      self.day = self.from.getDate();
     }
+
+    self.timestampe = self.from.getTime();
+    self.unixTimestampe = self.timestampe / 1000;
   };
 
   Calendar.init.prototype = Calendar.prototype;
